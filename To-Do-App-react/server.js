411d36b0f3f8ffb2e870d68d1f581e8758b36c7a
lyran10 <liranramekar629@gmail.com>
2022-08-07 23:14:13 +0300

    added

--- To-Do-App-react/server.js
+++ To-Do-App-react/server.js
@@ -8,6 +8,11 @@ app.use(bodyParser.urlencoded({extended:false}));
 app.use(bodyParser.json());
 app.use('/todo',router);
 
-app.listen(4000, () => {
-  console.log(`listening to 4000`)
+app.listen(process.env.PORT, () => {
+  console.log(`listening to ${process.env.PORT}`)
+})
+
+app.use("/",express.static(path.join(__dirname, "client/build")))
+app.get("*",(req,res) => {
+  res.sendFile(path.resolve(__dirname,"./client/build","index.html"))
 })
